3eef62625d3d03e5d7e9db6567e3eec97f6c2f23
Steven Hood <steven.hood@live.co.uk>
2015-06-10 14:18:50 +0100

    Add Part class representing segments of snake

--- snake.py
+++ snake.py
@@ -15,6 +15,22 @@ pygame.init()
 
 WINDOW_DIMENSIONS = (WINDOW_WIDTH, WINDOW_HEIGHT) = 640, 480
 
+class Part(Sprite):
+	def __init__(self, x, y, velocity_x, velocity_y):
+		Sprite.__init__(self)
+		self.image = pygame.Surface([5, 5])
+		self.image.fill(pygame.Color(0, 255, 0, 100)) # Green
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+		self.velocity_x = velocity_x
+		self.velocity_y = velocity_y
+
+	def update(self):
+		self.rect.move_ip(self.velocity_x, self.velocity_y)
+
+class Snake(Sprite):
+	pass
+
 def main():
 	screen = pygame.display.set_mode(WINDOW_DIMENSIONS)
 	pygame.display.set_caption('Snake')
@@ -23,6 +39,8 @@ def main():
 	screen.blit(background, (0, 0))
 
 	clock = pygame.time.Clock()
+	part = Part(10, 10, 1, 1)
+	sprites = pygame.sprite.Group([part])
 
 	def end():
 		sys.exit(0)
@@ -36,6 +54,11 @@ def main():
 	while running:
 		clock.tick(30)
 		pygame.display.set_caption('Snake :: {0:.2f} fps'.format(clock.get_fps()))
+
+		sprites.update()
+		sprites.draw(screen)
+		pygame.display.flip()
+		sprites.clear(screen, background)
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
